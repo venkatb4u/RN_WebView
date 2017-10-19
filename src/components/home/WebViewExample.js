bca104f2e45de0fe95d5a09c0a470a70161d19b8
Venkat <venkat.crescentian@gmail.com>
2017-10-19 23:42:06 +0530

    url update

--- src/components/home/WebViewExample.js
+++ src/components/home/WebViewExample.js
@@ -11,9 +11,14 @@ const WebViewExample = () => {
    return (
       <View style = {styles.container}>
          <WebView
+            // this is to check how webView in RN, supports ftp
             // source = {{ uri: require('../../webviews/view.html') }}
+
+            
             source = {{ uri: 'https://www.myharmony.com/en-in/' }}
-            // source = {{ uri: 'https://react-dnd.github.io/react-dnd/examples-chessboard-tutorial-app.html' }}
+
+            // this is to check how Drag-&-Drop in webview works
+            // source = {{ uri: 'https://react-dnd.github.io/react-dnd/examples-chessboard-tutorial-app.html' }} 
          />
       </View>
    )
